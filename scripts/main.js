e969ef877d086b37507ece7d0eb2634b5cd69eb8
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-18 00:01:31 +0200

    done js, comments added

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 
-//Creates variable for the carousel container
+//Creates variable for the carousel container.
 const carouselContainerElement = document.getElementById("carousel_container");
 
-//Creaes global variable to decide which image is visualized
+//Creates variables for carousel buttons
+const backBtnElement = document.getElementById("back_btn");
+const fwdBtnElement = document.getElementById("fwd_btn");
+
+//Creates global variable to decide which image is visualized (used later in "addEventListener").
 let currentImgIndex = 0;
 
-//Creates array of objects with carousel content
+//Creates array of objects with content to be put in carousel.
 const images = [
     {
         image: "img/01.webp",
@@ -30,27 +34,27 @@ const images = [
     }
 ];
 
-//"forEach" cycle cycles "images[]" and creates the carousel's content
+//"forEach" cycle cycles "images[]" and creates the carousel's content.
 images.forEach((element, i) => {
 
-    //Creates a variable that's an empty string by default
+    //Creates a variable that's an empty string by default.
     let imageClasses = "";
 
-    //"if...else" construct puts "active" in "imageClasses" if i === 0 (when the carousel is created)
+    //"if...else" construct puts "active" in "imageClasses" if i === 0 (when the carousel is created).
     if (i === 0) {
         imageClasses = "active";
     } else {
         imageClasses = null;
     }
 
-    /* Creates img container, appends in carousel and adds classes */
+    // Creates img container, appends in "carouselContainerElement" and adds classes.
     const carouselImgContainerElement = document.createElement("div");
 
     carouselContainerElement.append(carouselImgContainerElement);
 
     carouselImgContainerElement.classList.add("carousel_img_container", "position-absolute", "top-0", "start-0", imageClasses);
 
-    /* Creates img element, appends in carousel and adds src attribute and classes */
+    // Creates img element, appends in "carouselImgContainerElement", adds src attribute and classes.
     const carouselImgElement = document.createElement("img");
 
     carouselImgContainerElement.append(carouselImgElement);
@@ -59,25 +63,28 @@ images.forEach((element, i) => {
 
     carouselImgElement.classList.add("w-100", "h-100");
 
+    // Creates text container, appends in "carouselImgContainerElement", adds classes.
     const carouselTextContainerElement = document.createElement("div");
 
     carouselImgContainerElement.append(carouselTextContainerElement);
 
     carouselTextContainerElement.classList.add("carousel_text_container", "ps-2", "text-white");
 
+    // Creates title element, appends in "carouselTextContainerElement", adds classes.
     const carouselImgTitleElement = document.createElement("h4");
 
     carouselTextContainerElement.append(carouselImgTitleElement);
 
     carouselImgTitleElement.innerHTML += element.title;
 
+    // Creates paragraph element, appends in "carouselTextContainerElement", adds classes.
     const carouselImgTextElement = document.createElement("p");
 
     carouselTextContainerElement.append(carouselImgTextElement);
 
     carouselImgTextElement.innerHTML += element.text;
 
-    
+    //Code commented below does the same thing as above using "innerHtml" property.
 
     // carouselContainer.innerHTML += `<div class="carousel_img_container position-absolute top-0 start-0 ${imageClasses}">
     // <img src="${element.image}" alt="" class="w-100 h-100">
@@ -89,45 +96,57 @@ images.forEach((element, i) => {
 
 })
 
-const backBtn = document.getElementById("back_btn");
-const fwdBtn = document.getElementById("fwd_btn");
+//"addEventListener" for "fwdBtnElement", function defined later.
+fwdBtnElement.addEventListener("click", goToNxtImg);
 
-fwdBtn.addEventListener("click", goToNxtImg);
+//"addEventListener" for "backBtnElement", function defined later.
+backBtnElement.addEventListener("click", goToPreviousImg);
 
-backBtn.addEventListener("click", goToPreviousImg);
-
+//Function for "fwdBtnElement" "addEventListener".
 function goToNxtImg() {
 
+    //Creates a variable (an array) for ALL the "carouselImgContainerElement".
     const imgElements = document.querySelectorAll(".carousel_img_container");
 
+    //Removes ".active" from img visualized before "click".
     imgElements[currentImgIndex].classList.remove("active");
 
+    //Increases "currentImgIndex", used later to add ".active" to next img.
     currentImgIndex++;
 
+    //"if" construct sets "currentImgIndex" to 0 when clicks go over imgs number, goes back to first img.
     if (currentImgIndex > imgElements.length -1) {
 
         currentImgIndex = 0;
 
     }
 
+    //Adds ".active" to new visualized img.
     imgElements[currentImgIndex].classList.add("active");
 
 }
 
+//Function for "backBtnElement" "addEventListener".
 function goToPreviousImg() {
 
+    //Creates a variable (an array) for ALL the "carouselImgContainerElement".
     const imgElements = document.querySelectorAll(".carousel_img_container");
 
+    //Removes ".active" from img visualized before "click".
     imgElements[currentImgIndex].classList.remove("active");
 
+    //Decreases "currentImgIndex", used later to add ".active" to previous img.
     currentImgIndex--;
 
+    /*"if" construct sets "currentImgIndex" to ".length" of "imgElements" when "click" goes under 
+    imgs number, goes forward to last img. */
     if (currentImgIndex < 0) {
 
         currentImgIndex = imgElements.length - 1;
         
     }
 
+    //Adds ".active" to new visualized img.
     imgElements[currentImgIndex].classList.add("active");
 
 }
